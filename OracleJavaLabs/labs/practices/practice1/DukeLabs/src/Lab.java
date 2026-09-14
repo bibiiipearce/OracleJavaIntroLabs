09cf35b50568138d973b98912af884799376f1d1
bibiiipearce <bibipearce1@outlook.com>
2026-09-14 11:56:06 +0000

    Practice 1: creating my first java class

---
+++ OracleJavaLabs/labs/practices/practice1/DukeLabs/src/Lab.java
@@ -0,0 +1,10 @@
+public class Lab {
+    
+    public static void main(String[] args) {
+        // System.out.println("Hello, world!");
+        System.out.println("Hello " + args[0]);
+    }
+}
+
+// compile code, then type 'java Lab Bibi' and the output is 'Hello Bibi'
+// use 'Bibi Pearce' if you dont want Bibi and Pearce to be treated as seperate arguments
